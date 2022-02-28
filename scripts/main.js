29f305da08f9bb1f7f504f1b56fc8f443c8918a2
slimeeeee <57168340+slimeeeee@users.noreply.github.com>
2022-02-28 18:21:33 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
-function test(){
-    alert("a")
+function generate(){
+    var element = document.getElementById('input');
+    var text = element.value
+    var output = text
+    document.getElementById('output')=output
 }
