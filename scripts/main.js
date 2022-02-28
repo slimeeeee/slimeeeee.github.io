661749491a3bdbda03ce7530a29d518a4d61ad91
slimeeeee <57168340+slimeeeee@users.noreply.github.com>
2022-02-28 18:30:36 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 function generate(){
     var element = document.getElementById('input');
     var text = element.value
-    var output = document.getElementById('output').value
-    output=text
+    document.getElementById('output').value=text
 }
