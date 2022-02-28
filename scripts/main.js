dc30483985772db33f38b32698eb93903bcc9569
slimeeeee <57168340+slimeeeee@users.noreply.github.com>
2022-02-28 20:20:22 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,18 @@
+function const_condition(condition){
+    return{
+        condition: condition
+    }
+}
 function generate(){
     var element = document.getElementById('input');
     var text = element.value
-    document.getElementById('output').value=text
+    var list = text.split(\n)
+    var json={}
+    json.pools=[]
+    json.pools.push({})
+    json.pools[0].conditions=[]
+    json.pools[0].conditions.push(const_condition('killed_by_player'))
+    json.pools[0].rolls=1
+    json.pools[0].entries=[]
+    document.getElementById('output').value=list
 }
