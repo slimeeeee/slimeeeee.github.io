b3074f379faea3a8f91f03304fc6f359ab7e50e3
slimeeeee <57168340+slimeeeee@users.noreply.github.com>
2022-02-28 21:55:49 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,44 @@ function const_condition(condition){
         condition: condition
     }
 }
+function const_function(fun){
+    var obj={}
+    obj["function"]=fun
+    return obj
+}
+function const_item(item,weight,count,loot_count){
+    var ret = {}
+    ret.type="item"
+    ret.name=item
+    ret.weight=weight
+    ret.functions=[]
+    if(count.match(/\d+-\d+/)){
+        var count_min=count.match(/\d+/g)[0]
+        var count_max=count.match(/\d+/g)[1]
+        var set_count=const_function('set_count')
+        set_count.count={}
+        set_count.count.min=count_min
+        set_count.count.max=count_max
+        ret.functions.push(set_count)
+    }else{
+        alert('please use (min)-(max)')
+    }
+    if(loot_count.match(/\d+-\d+/)){
+        var loot_min=loot_count.match(/\d+/g)[0]
+        var loot_max=loot_count.match(/\d+/g)[1]
+        var looting_enchant_count=const_function('looting_enchant')
+        looting_enchant_count.count={}
+        looting_enchant_count.count.min=loot_min
+        looting_enchant_count.count.max=loot_max
+        ret.functions.push(looting_enchant_count)
+    }else{
+        alert('please use (min)-(max)')
+    }
+    return ret
+}
 function generate(){
-    var element = document.getElementById('input');
-    var text = element.value
+    var input = document.getElementById('input');
+    var text = input.value
     var list = text.split('\n')
     var json={}
     json.pools=[]
@@ -14,6 +49,12 @@ function generate(){
     json.pools[0].conditions.push(const_condition('killed_by_player'))
     json.pools[0].rolls=1
     json.pools[0].entries=[]
-    document.getElementById('output').value=list.join(',')
-    alert(list.join(','))
+    for(let i = 0;i<list.length;i++){
+        var target = list[i]
+        var arr=target.split(',')
+        var item_obj = const_item(arr[0],arr[1],arr[2],arr[3])
+        json.pools[0].entries.push(item_obj)
+    }
+    var output=JSON.stringify(json, null, 4)
+    document.getElementById('output').value=output
 }
